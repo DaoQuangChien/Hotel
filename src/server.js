f67b006aed201e33abca7afd1c1fa39cfac86ac9
Chien.Dao <quangchienspkt@gmail.com>
2017-10-20 08:03:13 +0700

    Change layout

--- src/server.js
+++ src/server.js
@@ -8,7 +8,6 @@ var express = require('express'),
 var app = express();
 
 var port = process.env.PORT || 3000;
-
 app.set('port', port);
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'jade');
